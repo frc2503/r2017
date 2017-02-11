29eccb097248a61486896a0195e6586a170e2de5
warrior-robotics <warriorrobotics@outlook.com>
2017-02-11 14:53:34 -0600

    Initial Vision Processing
    Runs at a smooth 4 fps.

--- src/org/usfirst/frc/team2503/robot/Robot.java
+++ src/org/usfirst/frc/team2503/robot/Robot.java
@@ -1,9 +1,38 @@
 package org.usfirst.frc.team2503.robot;
 
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+
+import org.opencv.core.Mat;
+
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
+	Thread visionThread;
+
+	@Override
 	public void robotInit() {
+		visionThread = new Thread(() -> {
+			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+			camera.setResolution(640, 480);
+			
+			Mat mat = new Mat();
+
+			CvSource outputStream = CameraServer.getInstance().putVideo("2503 Camera Stream", 640, 480);
+			CvSink cvSink = CameraServer.getInstance().getVideo();
+			
+			 while(!Thread.interrupted()) {
+                 cvSink.grabFrame(mat);
+                 outputStream.putFrame(mat);
+                 
+                 Vision.process(mat);
+             }
+		});
+		
+		visionThread.setDaemon(true);
+		visionThread.start();
 	}
 	
 	public void autonomousInit() {
@@ -16,10 +45,10 @@ public class Robot extends IterativeRobot {
 	}
 	
 	public void teleopPeriodic() {
-		Drive.teleop();
-		Intake.teleop();
-		Shooter.teleop();
-		//Shifter.teleop();
+		//BallIntake.teleop();
+		BallShooter.teleop();
+		//DriveBase.teleop();
+		//SuperShifter.teleop();
 	}
 }
 
